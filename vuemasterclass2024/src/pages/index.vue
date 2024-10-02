0a2d41ae057fc00ccd692fdc44c59ab232e93fac
jasmine-tea97 <jsmlee97@gmail.com>
2024-10-02 17:25:04 +0200

    setup auto routes in Vue Router and refactored codebase for file-based routing

---
+++ vuemasterclass2024/src/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Home Page</h1>
+    <RouterLink :to="{name: '/projects/[id]', params: {id: 1}}">Go to Project 1</RouterLink>
+</template>
--- vuemasterclass2024/src/router/index.ts
+++ vuemasterclass2024/src/router/index.ts
@@ -1,19 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import {routes} from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
