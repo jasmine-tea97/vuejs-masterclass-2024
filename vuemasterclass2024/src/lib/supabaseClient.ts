2882ed54d5d8609cec5f95c15210e2ec29eb4b3e
jasmine-tea97 <jsmlee97@gmail.com>
2024-10-04 15:02:40 +0200

    setup supabase environment variables

--- vuemasterclass2024/src/lib/supabaseClient.ts
+++ vuemasterclass2024/src/lib/supabaseClient.ts
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient('https://pfosyacbszmwxrkxaaaw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmb3N5YWNic3ptd3hya3hhYWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5NTcyNTgsImV4cCI6MjA0MzUzMzI1OH0.hk32iblNrg4NjWJvLDYH7Ht0RjKrxC3JaaJucN8PMgE')
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+)
